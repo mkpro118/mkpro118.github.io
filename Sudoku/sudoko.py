4d2bd2460129d24fb725520de27f43d887e2b1a2
M K <85588877+mkpro118@users.noreply.github.com>
2021-08-17 12:56:54 +0530

    Update sudoko.py

--- Sudoku/sudoko.py
+++ Sudoku/sudoko.py
@@ -25,31 +25,31 @@ def r(m,n,t=False):
 def sudokuGenerator():
     global sudo_list
     # Step 3: Shuffling Col 1-3
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(0,3,True)
     # Step 4: Shuffling Col 4-6
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(3,6,True)
     # Step 5: Shuffling Col 7-9
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(6,9,True)
     # Step 6: Shuffling Row 1-3
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(0,3)
     # Step 7: Shuffling Row 4-6
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(3,6)
     # Step 8: Shuffling Row 7-9
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         r(6,9)
     # Step 9: Shuffling rows in sets of 3
     rows = np.array_split(sudo_list, 3)
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         random.shuffle(rows)
     sudo_list = np.vstack(rows)
     # Step 10: Shuffling columns in sets of 3
     col = np.array_split(sudo_list.T, 3)
-    for i in range(random.randint(5, 10)):
+    for _ in range(random.randint(5, 10)):
         random.shuffle(col)
     sudo_list = np.vstack(col)
 
